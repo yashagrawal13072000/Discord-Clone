34785768297eabeec09987388749e0035fe85612
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-07-17 00:11:07 -0400

    1st Update

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+const state = true
+
+export default function Home() {
+  return (
+    <div>
+    This is a protected route
+    </div>
+  );
+}
