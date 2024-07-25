1f9cfa7ed740294aace8703bd7d1cbcdacdffdc6
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-07-25 14:57:33 -0400

    Server Creation API

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  publicRoutes: ["/api/uploadthing"]
+  
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
