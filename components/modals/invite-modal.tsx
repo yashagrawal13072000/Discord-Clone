b5c67b4bebb17a67cf08071df1abc018e0b3bdae
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-06 19:18:21 -0400

    Invitation Completed!

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -157,10 +157,11 @@ import { Button } from "@/components/ui/button";
 import { Check, Copy, RefreshCw } from "lucide-react";
 import { useOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
+import axios from "axios";
 
 
 export const InviteModal = () => {
-    const { isOpen, onClose, type, data } = useModal();
+    const { onOpen, isOpen, onClose, type, data } = useModal();
     const origin = useOrigin();
 
     const isModalOpen = isOpen && type === "invite";
@@ -178,6 +179,19 @@ export const InviteModal = () => {
         setTimeout(() => {
             setCopied(false);
         }, 1000);
+    };
+
+    const onNew = async () => {
+        try{
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+
+            onOpen("invite", { server: response.data });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
     }
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -195,10 +209,11 @@ export const InviteModal = () => {
                 </Label>
                 <div className="flex items-center mt-2 gap-x-2">
                     <Input
+                    disabled={isLoading}
                     className="bg-zinc-300/50 border-0 
                     focus-visible:ring-0 text-black focus-visible:ring-offset-0"
                     value={inviteUrl} />
-                    <Button onClick={onCopy} size="icon">
+                    <Button disabled={isLoading} onClick={onCopy} size="icon">
                         {copied 
                         ? <Check className="w-4 h-4" /> 
                         : <Copy className="w-4 h-4" /> 
@@ -206,6 +221,8 @@ export const InviteModal = () => {
                     </Button>
                 </div>
                 <Button
+                onClick={onNew}
+                disabled={isLoading}
                 variant="link"
                 size="sm"
                 className="text-xs text-zinc-500 mt-4">
