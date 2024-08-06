960d08f390cfe5b794560ff4d702bb6a95c09b0f
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-06 19:34:57 -0400

    Server Settings Completed!

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -62,6 +62,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem 
+                    onClick={() => onOpen("editServer", { server })}
                     className="px-3 py-2 text-sm cursor-pointer">
                         Server Setting
                         <Settings className="h-4 w-4 ml-auto"/>
