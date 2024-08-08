bf96de56bf2e7460b02935c51227798d2dbcbcf1
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-07 21:00:21 -0400

    Making guests moderator!

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -63,7 +63,6 @@ export const MembersModal = () => {
                 url: `/api/members/${memberId}`,
                 query: {
                     serverId: server?.id,
-                    memberId,
                 }
             });
 
