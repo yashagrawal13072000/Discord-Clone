a52214fac78d75857ddb24084dcb18e54c9eb4c2
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-10 02:31:38 -0400

    Delete Server Completed

--- app/api/servers/[serverId]/leave/route.ts
+++ app/api/servers/[serverId]/leave/route.ts
@@ -41,6 +41,6 @@ export async function PATCH(
         return NextResponse.json(server)
     } catch (error) {
         console.log("[SERVER_ID_LEAVE]", error);
-        return new NextResponse("Internal Error", {status: 500})
+        return new NextResponse("Internal Error", {status: 500});
     }
 }
--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,32 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function DELETE(
+    req: Request,
+    { params }: { params: { serverId: string}}
+) {
+    try{
+        const profile = await currentProfile();
+
+        if (!profile) {
+            return new NextResponse("Unauthorized", { status: 401});
+        }
+
+        const server = await db.server.delete({
+            where: {
+                id: params.serverId,
+                profileId: profile.id,
+            },
+        });
+
+        return NextResponse.json(server);
+
+    } catch (error) {
+        console.log("[SERVER_ID_DELETE",error);
+        return new NextResponse("internal Error", { status: 500 });
+    }
+}
+
 export async function PATCH(
     req: Request,
     { params }: { params: { serverId: string}}
@@ -17,6 +43,7 @@ export async function PATCH(
         const server = await db.server.update({
             where: {
                 id: params.serverId,
+                profileId: profile.id,
             },
             data: {
                 name,
