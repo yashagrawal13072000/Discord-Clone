1378a486005dd5548d0953135ee3b8895befd826
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-09 20:35:21 -0400

    Leave Server Completed!

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -96,6 +96,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem 
+                    onClick={() => onOpen("leaveServer", { server })}
                     className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/>
