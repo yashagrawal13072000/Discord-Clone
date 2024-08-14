2608e0404c1c2686abae340a5db236361413d270
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-13 21:02:02 -0400

    Channel ID Page Completed!

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -25,8 +25,13 @@ export const ServerMember = ({
     const router = useRouter();
 
     const icon = roleIconMap[member.role];
+
+    const onClick = () => {
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+    }
     return (
-        <button 
+        <button
+        onClick={onClick} 
         className={cn(
             "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
             params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700 "
@@ -34,7 +39,7 @@ export const ServerMember = ({
             <UserAvatar src={member.profile.imageUrl}
             className="h-8 w-8 md:h-8 md:w-8" />
             <p className={cn("font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-            params?.channelId === member.id && "text-primary")}>
+            params?.memberId === member.id && "text-primary")}>
               {member.profile.name}  
             </p>
             
