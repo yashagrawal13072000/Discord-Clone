358dbb5e67cd0cb5030f5a4b73ecc109588d8c4b
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-24 00:25:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,19 +10,5 @@ const nextConfig = {
         ],
     },
 };
-//     webpack: (config) => {
-//         config.externals.push({
-//             "utf-8-validate": "commonjs utf-8-validate",
-//             bufferutil: "commonjs bufferutil"
-//         });
-//         return config;
-//     },
-//     images: {
-//         domains: [
-//             "utfs.io"
-//         ]
-//     }
-// };
-
 
 export default nextConfig;
