a143e51657340208a9da6e0b7fa26cc9b25bb465
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-24 00:23:33 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.externals.push({
-            "utf-8-validate": "commonjs utf-8-validate",
-            bufferutil: "commonjs bufferutil"
-        });
-        return config;
-    },
     images: {
-        domains: [
-            "utfs.io"
-        ]
-    }
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "utfs.io"
+            },
+        ],
+    },
 };
+//     webpack: (config) => {
+//         config.externals.push({
+//             "utf-8-validate": "commonjs utf-8-validate",
+//             bufferutil: "commonjs bufferutil"
+//         });
+//         return config;
+//     },
+//     images: {
+//         domains: [
+//             "utfs.io"
+//         ]
+//     }
+// };
+
 
 export default nextConfig;
