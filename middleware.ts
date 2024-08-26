21188dbdd34e50fa794b9508546b2e68f531dd4b
yashagrawal13072000 <143053630+yashagrawal13072000@users.noreply.github.com>
2024-08-26 14:31:11 -0400

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,27 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/uploadthing"]
+// export default clerkMiddleware({
+//   publicRoutes: ["/api/uploadthing"]
   
-});
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isProtectedRoute = createRouteMatcher(['/'])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const nextConfig = {
     images: {
         remotePatterns: [
             {
-                protocol: "https",
                 hostname: "utfs.io"
             },
         ],
